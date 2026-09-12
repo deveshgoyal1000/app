bbe8525a262c5728b9bd75cbd7c9446b25f7beb5
deveshgoyal1000 <devesh.21bcon427@jecrcu.edu.in>
2026-09-12 19:36:31 +0530

    fix(ios): completely obliterate generic Expo grid from iOS native launch screen too

--- withBlankSplash.js
+++ withBlankSplash.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function withBlankSplash(config) {
-  return withDangerousMod(config, [
+  // Android Mod
+  config = withDangerousMod(config, [
     'android',
     (config) => {
       const resDir = path.join(config.modRequest.platformProjectRoot, 'app/src/main/res');
@@ -22,4 +23,27 @@ module.exports = function withBlankSplash(config) {
       return config;
     },
   ]);
+
+  // iOS Mod
+  config = withDangerousMod(config, [
+    'ios',
+    (config) => {
+      const projectName = config.modRequest.projectName;
+      const imagesetDir = path.join(config.modRequest.platformProjectRoot, projectName, 'Images.xcassets', 'SplashScreen.imageset');
+      const transparentPng = Buffer.from('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=', 'base64');
+      
+      if (fs.existsSync(imagesetDir)) {
+        const files = fs.readdirSync(imagesetDir);
+        files.forEach(file => {
+          if (file.endsWith('.png')) {
+            const logoPath = path.join(imagesetDir, file);
+            fs.writeFileSync(logoPath, transparentPng);
+          }
+        });
+      }
+      return config;
+    },
+  ]);
+
+  return config;
 };
