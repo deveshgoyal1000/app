2984c1b0a1fb19a40520c7c562d0b6b2a7ef5969
deveshgoyal1000 <devesh.21bcon427@jecrcu.edu.in>
2026-09-12 13:02:24 +0530

    fix(splash): perfectly blank native splash to allow JS animated logo to run without grid glitch

---
+++ withBlankSplash.js
@@ -0,0 +1,25 @@
+﻿const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function withBlankSplash(config) {
+  return withDangerousMod(config, [
+    'android',
+    (config) => {
+      const resDir = path.join(config.modRequest.platformProjectRoot, 'app/src/main/res');
+      const transparentPng = Buffer.from('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=', 'base64');
+      if (fs.existsSync(resDir)) {
+        const folders = fs.readdirSync(resDir);
+        folders.forEach(folder => {
+          if (folder.startsWith('drawable') || folder.startsWith('mipmap')) {
+            const logoPath = path.join(resDir, folder, 'splashscreen_logo.png');
+            if (fs.existsSync(logoPath)) {
+              fs.writeFileSync(logoPath, transparentPng);
+            }
+          }
+        });
+      }
+      return config;
+    },
+  ]);
+};
